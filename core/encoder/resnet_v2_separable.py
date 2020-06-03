28a25720a161fed0727801d32bff5256704c51c8
cinderellab <cinderela80@proton.me>
2020-06-03 08:56:00 +0000

    Update and rename index.php to index.html

--- core/encoder/resnet_v2_separable.py
+++ core/encoder/resnet_v2_separable.py
@@ -1,3 +1,6 @@
 from keras.layers import Input
 from keras.layers.core import Activation
-from keras.layers.convolutional import 
+from keras.layers.convolutional import Conv2D, SeparableConv2D
+from keras.layers.merge import Add
+from keras.layers.normalization import BatchNormalization
+from keras.layers.pooling import MaxPo
