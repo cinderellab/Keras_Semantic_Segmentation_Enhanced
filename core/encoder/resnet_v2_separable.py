b5ed2595080f44c0a31c1fa64e9dd94018d7bd5a
cinderellab <cinderela80@proton.me>
2020-06-05 21:24:02 +0000

    1.3.0

--- core/encoder/resnet_v2_separable.py
+++ core/encoder/resnet_v2_separable.py
@@ -3,4 +3,11 @@ from keras.layers.core import Activation
 from keras.layers.convolutional import Conv2D, SeparableConv2D
 from keras.layers.merge import Add
 from keras.layers.normalization import BatchNormalization
-from keras.layers.pooling import MaxPo
+from keras.layers.pooling import MaxPooling2D
+from keras.models import Model
+from keras.regularizers import l2
+
+
+def residual_block(inputs,
+                   base_depth,
+           
