d5466297c8d493e6573beccbd11822f9777144e4
cinderellab <cinderela80@proton.me>
2020-07-20 13:30:17 +0000

    Logging change

--- core/encoder/resnet_v2_separable.py
+++ core/encoder/resnet_v2_separable.py
@@ -172,4 +172,6 @@ def resnet_v2_50_separable(input_shape,
     x = residual_bottleneck(x, bottleneck_param(scope="block1", base_depth=64, kernel_size=kernel_size,
                                                 num_units=3, stride=2, rate=1),
                             weight_decay=weight_decay, kernel_initializer=kernel_initializer,
-                            bn_epsilon=bn_epsilon, bn_
+                            bn_epsilon=bn_epsilon, bn_momentum=bn_momentum)
+    x = residual_bottleneck(x, bottleneck_param(scope="block2", base_depth=128, kernel_size=kernel_size,
+           
