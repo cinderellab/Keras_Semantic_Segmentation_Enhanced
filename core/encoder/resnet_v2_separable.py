a197a4843f14cae663115173e54449b0c0f5dd98
cinderellab <cinderela80@proton.me>
2020-07-26 05:41:28 +0000

    fix deploy

--- core/encoder/resnet_v2_separable.py
+++ core/encoder/resnet_v2_separable.py
@@ -199,3 +199,7 @@ def resnet_v2_101_separable(input_shape,
                             kernel_initializer="he_normal",
                             bn_epsilon=1e-3,
                             bn_momentum=0.99):
+    """ Build a Resnet_v2_101_separable encoder
+    :param input_shape: tuple, i.e., (height, width, channel)
+    :param kernel_size: int, default 3.
+    :param incl
