af11e424ff04c69b5faa66c302396a854537618a
cinderellab <cinderela80@proton.me>
2020-07-26 11:08:44 +0000

    Nominate release candidate

--- core/encoder/resnet_v2_separable.py
+++ core/encoder/resnet_v2_separable.py
@@ -204,4 +204,10 @@ def resnet_v2_101_separable(input_shape,
     :param kernel_size: int, default 3.
     :param include_root: bool, default True.
     :param weight_decay: float, default 1e-4.
-    :param kernel_initializer: string, default "he_normal
+    :param kernel_initializer: string, default "he_normal".
+    :param bn_epsilon: float, default 1e-3.
+    :param bn_momentum: float, default 0.99.
+
+    :return: a Keras model instance.
+    """
+    input_x = Input(sha
