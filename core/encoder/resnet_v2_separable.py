1cba93c042d45ca86bef6f986c0d2bf5e8d38a5b
cinderellab <cinderela80@proton.me>
2020-08-05 12:54:00 +0000

    mm: mmu_notifier: make the mmu_notifier srcu static

--- core/encoder/resnet_v2_separable.py
+++ core/encoder/resnet_v2_separable.py
@@ -231,4 +231,5 @@ def resnet_v2_101_separable(input_shape,
                                                 num_units=23, stride=1, rate=2),
                             weight_decay=weight_decay, kernel_initializer=kernel_initializer,
                             bn_epsilon=bn_epsilon, bn_momentum=bn_momentum)
-    x = residual_bottleneck(x, bott
+    x = residual_bottleneck(x, bottleneck_param(scope="block4", base_depth=512, kernel_size=kernel_size,
+                                                num_unit
