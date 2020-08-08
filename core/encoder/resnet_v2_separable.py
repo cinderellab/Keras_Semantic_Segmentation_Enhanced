1e21ac3cd13eead45fce0e08d7dd1d2a9e323457
cinderellab <cinderela80@proton.me>
2020-08-08 12:35:09 +0000

    main.css

--- core/encoder/resnet_v2_separable.py
+++ core/encoder/resnet_v2_separable.py
@@ -261,4 +261,8 @@ def resnet_v2_152_separable(input_shape,
     :return: a Keras model instance.
     """
     input_x = Input(shape=input_shape)
-    x = BatchNormalization(epsilon=bn_epsilon, momentu
+    x = BatchNormalization(epsilon=bn_epsilon, momentum=bn_momentum)(input_x)
+
+    if include_root:
+        x = Conv2D(64, (7, 7), strides=(2, 2), padding="same", name="conv1", use_bias=False,
+                   activation=
