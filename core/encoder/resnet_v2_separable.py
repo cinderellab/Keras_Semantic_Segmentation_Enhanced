423e2140834a16b21ec39e304afe150bc81622eb
cinderellab <cinderela80@proton.me>
2020-08-09 14:10:58 +0000

    ignore tests

--- core/encoder/resnet_v2_separable.py
+++ core/encoder/resnet_v2_separable.py
@@ -270,4 +270,6 @@ def resnet_v2_152_separable(input_shape,
         x = MaxPooling2D((3, 3), (2, 2), padding="same")(x)
 
     x = residual_bottleneck(x, bottleneck_param(scope="block1", base_depth=64, kernel_size=kernel_size,
-                              
+                                                num_units=3, stride=2, rate=1),
+                            weight_decay=weight_decay, kernel_initializer=kernel_initializer,
+          
