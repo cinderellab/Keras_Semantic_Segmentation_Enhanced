ffc88d2200d32546497a4ee0502c079097499a5a
cinderellab <cinderela80@proton.me>
2020-08-18 06:19:40 +0000

    Added travis-ci support

--- core/encoder/resnet_v2_separable.py
+++ core/encoder/resnet_v2_separable.py
@@ -284,4 +284,7 @@ def resnet_v2_152_separable(input_shape,
     x = residual_bottleneck(x, bottleneck_param(scope="block4", base_depth=512, kernel_size=kernel_size,
                                                 num_units=3, stride=1, rate=4),
                             weight_decay=weight_decay, kernel_initializer=kernel_initializer,
-                      
+                            bn_epsilon=bn_epsilon, bn_momentum=bn_momentum)
+
+    x = BatchNormalization(epsilon=bn_epsilon, momentum=bn_momentum)(x)
+    x = Activation("relu")(x)
