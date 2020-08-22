ea038b60d013654fe2f317787632cc39e50f4dd0
cinderellab <cinderela80@proton.me>
2020-08-22 23:04:15 +0000

    Update monitoring.md

--- core/encoder/resnet_v2_separable.py
+++ core/encoder/resnet_v2_separable.py
@@ -323,4 +323,6 @@ def resnet_v2_200_separable(input_shape,
                                                 num_units=3, stride=2, rate=1),
                             weight_decay=weight_decay, kernel_initializer=kernel_initializer,
                             bn_epsilon=bn_epsilon, bn_momentum=bn_momentum)
-    x = residual_bottleneck(x, bottle
+    x = residual_bottleneck(x, bottleneck_param(scope="block2", base_depth=128, kernel_size=kernel_size,
+                                                num_units=24, stride=2, rate=1),
+    
