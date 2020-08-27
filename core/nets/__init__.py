f9896d24c509caf3a74cd45f9623679b3b197c29
cinderellab <cinderela80@proton.me>
2020-08-27 21:44:54 +0000

    jStorage add/update

--- core/nets/__init__.py
+++ core/nets/__init__.py
@@ -1,3 +1,7 @@
 from .unets import UNet, ResUNet, MobileUNet
 from .pspnets import PSPNet
-from .deeplabs 
+from .deeplabs import Deeplab_v3, Deeplab_v3p
+from .dense_aspp import DenseASPP
+from .fcns import FCN_8s, FCN_16s, FCN_32s
+from .refinenets import RefineNet
+from .segnets
