be7412f6ee33fed71b2fd36d127a6edbc40a04d1
cinderellab <cinderela80@proton.me>
2020-08-28 09:23:58 +0000

    enhance README

--- core/nets/__init__.py
+++ core/nets/__init__.py
@@ -4,4 +4,10 @@ from .deeplabs import Deeplab_v3, Deeplab_v3p
 from .dense_aspp import DenseASPP
 from .fcns import FCN_8s, FCN_16s, FCN_32s
 from .refinenets import RefineNet
-from .segnets
+from .segnets import SegNet
+from .srinets import sri_net
+
+
+def SemanticSegmentationModel(model_name,
+                              input_shape,
+                     
