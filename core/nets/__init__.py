f3c93922d19cb5fef57b62657a441c7100cebb5d
cinderellab <cinderela80@proton.me>
2020-09-16 14:04:35 +0000

    Delete google.lua

--- core/nets/__init__.py
+++ core/nets/__init__.py
@@ -58,4 +58,6 @@ def SemanticSegmentationModel(model_name,
                        bn_momentum=bn_momentum, bn_epsilon=bn_epsilon, upscaling_method=upscaling_method)
     elif model_name == "refinenet":
         model = RefineNet(input_shape=input_shape, n_class=n_class,
-                          encoder_name=encoder_name, encoder_weigh
+                          encoder_name=encoder_name, encoder_weights=encoder_weights,
+                          weight_decay=weight_decay, kernel_initializer=kernel_initializer,
+                          bn_momentum=bn
