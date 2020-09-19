25e5396cca14c611ccbb96e15babaaa1d9250aba
cinderellab <cinderela80@proton.me>
2020-09-19 15:42:02 +0000

    HID: i2c-hid: fix race condition reading reports

--- core/nets/__init__.py
+++ core/nets/__init__.py
@@ -69,4 +69,5 @@ def SemanticSegmentationModel(model_name,
                            bn_momentum=bn_momentum, bn_epsilon=bn_epsilon)
     elif model_name == "deeplab_v3p":
         model = Deeplab_v3p(input_shape=input_shape, n_class=n_class,
-                    
+                            encoder_name=encoder_name, encoder_weights=encoder_weights,
+                            weight_decay=weight_decay, kernel_initializer=kernel_initia
