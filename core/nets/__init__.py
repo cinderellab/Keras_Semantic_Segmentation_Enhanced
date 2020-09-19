0893b46ea8b66fd2fd04f5ed45c7f3669e547be0
cinderellab <cinderela80@proton.me>
2020-09-19 17:23:12 +0000

    Infinite Scrolling

--- core/nets/__init__.py
+++ core/nets/__init__.py
@@ -70,4 +70,7 @@ def SemanticSegmentationModel(model_name,
     elif model_name == "deeplab_v3p":
         model = Deeplab_v3p(input_shape=input_shape, n_class=n_class,
                             encoder_name=encoder_name, encoder_weights=encoder_weights,
-                            weight_decay=weight_decay, kernel_initializer=kernel_initia
+                            weight_decay=weight_decay, kernel_initializer=kernel_initializer,
+                            bn_momentum=bn_momentum, bn_epsilon=bn_epsilon)
+    elif model_name == "dense_aspp":
+        model = DenseASPP(input_shape=input_sh
