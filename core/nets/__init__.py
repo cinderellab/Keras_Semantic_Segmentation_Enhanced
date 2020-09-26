2fb68e09aed81bcc635d70bfe47c3c4a2c75429c
cinderellab <cinderela80@proton.me>
2020-09-26 23:04:26 +0000

    modified comments

--- core/nets/__init__.py
+++ core/nets/__init__.py
@@ -84,4 +84,6 @@ def SemanticSegmentationModel(model_name,
                        bn_momentum=bn_momentum, bn_epsilon=bn_epsilon, dropout=dropout)
     elif model_name == "fcn_16s":
         model = FCN_16s(input_shape=input_shape, n_class=n_class,
-                        encoder_
+                        encoder_name=encoder_name, encoder_weights=encoder_weights,
+                        weight_decay=weight_decay, kernel_initializer=kernel_initializer,
+                        b
