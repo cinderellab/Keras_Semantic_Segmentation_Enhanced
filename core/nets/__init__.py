a490919de97757191b1a2466ccabafcdb6b59f20
cinderellab <cinderela80@proton.me>
2020-09-28 11:24:29 +0000

    bump nfsbroker

--- core/nets/__init__.py
+++ core/nets/__init__.py
@@ -90,4 +90,6 @@ def SemanticSegmentationModel(model_name,
     elif model_name == "fcn_32s":
         model = FCN_32s(input_shape=input_shape, n_class=n_class,
                         encoder_name=encoder_name, encoder_weights=encoder_weights,
-                        weight_decay=weight_decay, kernel_initialize
+                        weight_decay=weight_decay, kernel_initializer=kernel_initializer,
+                        bn_momentum=bn_momentum, bn_epsilon=bn_epsilon, dropout=dropout)
+    elif model_name == "segnet
