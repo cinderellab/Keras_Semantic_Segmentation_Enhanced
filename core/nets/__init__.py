0cfdabe13cf03001a27e421a8d3c0b066415ab51
cinderellab <cinderela80@proton.me>
2020-09-29 08:55:41 +0000

    Update mascm.sh

--- core/nets/__init__.py
+++ core/nets/__init__.py
@@ -97,4 +97,7 @@ def SemanticSegmentationModel(model_name,
                        encoder_name=encoder_name, encoder_weights=encoder_weights,
                        weight_decay=weight_decay, kernel_initializer=kernel_initializer,
                        bn_momentum=bn_momentum, bn_epsilon=bn_epsilon)
-    elif model
+    elif model_name == "srinet":
+        model = sri_net(input_shape=input_shape, n_class=n_class,
+                        encoder_name=encoder_name, encoder_weights=encoder_weights,
+    
