f6ff795996779a5e22fff0eb64e539a8eba47f23
cinderellab <cinderela80@proton.me>
2020-10-07 17:13:46 +0000

    Updated Galician translations

--- core/nets/unets.py
+++ core/nets/unets.py
@@ -1,2 +1,4 @@
 from keras.engine import Input
-from keras.layers.
+from keras.layers.convolutional import Conv2D, Conv2DTranspose, SeparableConv2D
+from keras.layers.pooling import MaxPooling2D
+from keras.layers.core 
