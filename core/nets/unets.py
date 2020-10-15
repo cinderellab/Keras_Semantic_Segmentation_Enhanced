cae0215584289d1e0009d806ac0acf44a16f86f6
cinderellab <cinderela80@proton.me>
2020-10-15 20:26:37 +0000

    Add example.

--- core/nets/unets.py
+++ core/nets/unets.py
@@ -4,4 +4,11 @@ from keras.layers.pooling import MaxPooling2D
 from keras.layers.core import Dropout, Activation
 from keras.layers.normalization import BatchNormalization
 from keras.layers.merge import Concatenate, Add
-from 
+from keras.models import Model
+from keras.regularizers import l2
+
+from ..utils.net_utils import conv_bn_act_block, bn_act_convtranspose
+
+
+def UNet(input_shape,
+     
