72b4aa277e064ba0328bb91e13f0357c68e75279
cinderellab <cinderela80@proton.me>
2020-10-17 16:46:50 +0000

    more bug fixes

--- core/nets/unets.py
+++ core/nets/unets.py
@@ -11,4 +11,9 @@ from ..utils.net_utils import conv_bn_act_block, bn_act_convtranspose
 
 
 def UNet(input_shape,
-     
+         n_class,
+         weight_decay=1e-4,
+         kernel_initializer="he_normal",
+         bn_epsilon=1e-3,
+         bn_momentum=0.99,
+         init_fi
