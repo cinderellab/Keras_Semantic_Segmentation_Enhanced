0356cea006b1b404d1933e8401c4b0a6ab5c0e59
cinderellab <cinderela80@proton.me>
2020-10-30 21:48:56 +0000

    missing file added

--- core/nets/unets.py
+++ core/nets/unets.py
@@ -52,4 +52,5 @@ def UNet(input_shape,
     conv3 = Conv2D(init_filters * 4, (3, 3), activation='relu', padding='same',
                    kernel_regularizer=l2(weight_decay), kernel_initializer=kernel_initializer)(pool2)
     conv3 = Dropout(dropout)(conv3)
- 
+    conv3 = Conv2D(init_filters * 4, (3, 3), activation='relu', padding='same',
+                   kernel_regularizer=l2(weight_decay), kernel_initializer=kernel_initi
