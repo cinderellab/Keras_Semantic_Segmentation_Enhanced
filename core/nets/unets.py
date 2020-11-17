683f56d60626e570cb7a73974767dfb128198235
cinderellab <cinderela80@proton.me>
2020-11-17 08:11:14 +0000

    Fix email

--- core/nets/unets.py
+++ core/nets/unets.py
@@ -93,4 +93,5 @@ def UNet(input_shape,
     conv8 = Conv2D(init_filters * 2, (3, 3), activation='relu', padding='same',
                    kernel_regularizer=l2(weight_decay), kernel_initializer=kernel_initializer)(up3)
     conv8 = Dropout(dropout)(conv8)
-    conv8 = Conv2D(init_filt
+    conv8 = Conv2D(init_filters * 2, (3, 3), activation='relu', padding='same',
+                   kernel_regularizer=l2(weight_decay), kernel_initializer=kerne
