9ce46591fd96e2bfcaacefae8d28f9e84ddcff2a
cinderellab <cinderela80@proton.me>
2021-01-10 12:07:01 +0000

    [1] Bumped release.

--- core/nets/unets.py
+++ core/nets/unets.py
@@ -302,4 +302,8 @@ def MobileUNet(input_shape,
     x = DepthwiseSeparableConvBlock(x, 128, weight_decay=weight_decay, kernel_initializer=kernel_initializer,
                                     bn_epsilon=bn_epsilon, bn_momentum=bn_momentum)
     x = DepthwiseSeparableConvBlock(x, 128, weight_decay=weight_decay, kernel_initializer=kernel_initializer,
-                 
+                                    bn_epsilon=bn_epsilon, bn_momentum=bn_momentum)
+    x = MaxPooling2D()(x)
+    skip_2 = x
+
+    x = DepthwiseSeparableConvBlock(x, 256, weight_decay=w
