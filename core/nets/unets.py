470196699ffff67bc04c3069e8de0f7844c475ef
cinderellab <cinderela80@proton.me>
2021-01-17 16:48:47 +0000

    add more tests

--- core/nets/unets.py
+++ core/nets/unets.py
@@ -309,4 +309,6 @@ def MobileUNet(input_shape,
     x = DepthwiseSeparableConvBlock(x, 256, weight_decay=weight_decay, kernel_initializer=kernel_initializer,
                                     bn_epsilon=bn_epsilon, bn_momentum=bn_momentum)
     x = DepthwiseSeparableConvBlock(x, 256, weight_decay=weight_decay, kernel_initializer=kernel_initializer,
-                                    bn
+                                    bn_epsilon=bn_epsilon, bn_momentum=bn_momentum)
+    x = DepthwiseSeparableConvBlock(x, 256, weight_decay=weight_decay, kernel_initializer=kernel_initializer,
+       
