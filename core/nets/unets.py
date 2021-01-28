ffa2b67fa46ce54cf6cb9203f3cafe466822b55d
cinderellab <cinderela80@proton.me>
2021-01-28 18:49:09 +0000

    Fix events

--- core/nets/unets.py
+++ core/nets/unets.py
@@ -330,4 +330,7 @@ def MobileUNet(input_shape,
                                     bn_epsilon=bn_epsilon, bn_momentum=bn_momentum)
     x = DepthwiseSeparableConvBlock(x, 512, weight_decay=weight_decay, kernel_initializer=kernel_initializer,
                                     bn_epsilon=bn_epsilon, bn_momentum=bn_momentum)
-    x 
+    x = MaxPooling2D()(x)
+
+    x = bn_act_convtranspose(x, 512, kernel_size=3, scale=2, weight_decay=weight_decay,
+                             kernel_initializer=kern
