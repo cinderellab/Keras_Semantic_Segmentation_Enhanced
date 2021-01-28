737d4f7807a531211bfbaeceb6fb945b77cd7a39
cinderellab <cinderela80@proton.me>
2021-01-28 20:28:02 +0000

    Updating MCM tour

--- core/nets/unets.py
+++ core/nets/unets.py
@@ -334,4 +334,6 @@ def MobileUNet(input_shape,
 
     x = bn_act_convtranspose(x, 512, kernel_size=3, scale=2, weight_decay=weight_decay,
                              kernel_initializer=kernel_initializer, bn_epsilon=bn_epsilon, bn_momentum=bn_momentum)
-    x = DepthwiseSeparableConvBlock(x, 512, weight_decay=weight_decay, kernel_ini
+    x = DepthwiseSeparableConvBlock(x, 512, weight_decay=weight_decay, kernel_initializer=kernel_initializer,
+                                    bn_epsilon=bn_epsilon, bn_momentum=bn_momentum)
+    x = DepthwiseSeparableConvBlock(x, 512, weight_de
