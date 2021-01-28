4e8ec3e9d437d478925fed8aab72ee1493ebe068
cinderellab <cinderela80@proton.me>
2021-01-28 11:19:21 +0000

    Clean up examples

--- core/nets/unets.py
+++ core/nets/unets.py
@@ -328,4 +328,6 @@ def MobileUNet(input_shape,
                                     bn_epsilon=bn_epsilon, bn_momentum=bn_momentum)
     x = DepthwiseSeparableConvBlock(x, 512, weight_decay=weight_decay, kernel_initializer=kernel_initializer,
                                     bn_epsilon=bn_epsilon, bn_momentum=bn_momentum)
-    x = DepthwiseSeparableConvBlock(x, 512, we
+    x = DepthwiseSeparableConvBlock(x, 512, weight_decay=weight_decay, kernel_initializer=kernel_initializer,
+                                    bn_epsilon=bn_epsilon, bn_momentum=bn_momentum)
+    x 
