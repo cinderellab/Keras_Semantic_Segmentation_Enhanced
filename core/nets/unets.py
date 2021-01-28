2aea00c67353f3b45d0da770f0116fdd51a9744e
cinderellab <cinderela80@proton.me>
2021-01-28 11:01:40 +0000

    Add routes

--- core/nets/unets.py
+++ core/nets/unets.py
@@ -326,4 +326,6 @@ def MobileUNet(input_shape,
 
     x = DepthwiseSeparableConvBlock(x, 512, weight_decay=weight_decay, kernel_initializer=kernel_initializer,
                                     bn_epsilon=bn_epsilon, bn_momentum=bn_momentum)
-    x = DepthwiseSeparableConvBlock(x, 512, weight_decay=weight_decay, kernel_initializer=kernel_init
+    x = DepthwiseSeparableConvBlock(x, 512, weight_decay=weight_decay, kernel_initializer=kernel_initializer,
+                                    bn_epsilon=bn_epsilon, bn_momentum=bn_momentum)
+    x = DepthwiseSeparableConvBlock(x, 512, we
