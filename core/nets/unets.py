d8ec79804bf40c3efdfaf4b389187cddd09eaadb
cinderellab <cinderela80@proton.me>
2021-01-28 22:13:57 +0000

    Bump version to 0.8.1

--- core/nets/unets.py
+++ core/nets/unets.py
@@ -336,4 +336,5 @@ def MobileUNet(input_shape,
                              kernel_initializer=kernel_initializer, bn_epsilon=bn_epsilon, bn_momentum=bn_momentum)
     x = DepthwiseSeparableConvBlock(x, 512, weight_decay=weight_decay, kernel_initializer=kernel_initializer,
                                     bn_epsilon=bn_epsilon, bn_momentum=bn_momentum)
-    x = DepthwiseSeparableConvBlock(x, 512, weight_de
+    x = DepthwiseSeparableConvBlock(x, 512, weight_decay=weight_decay, kernel_initializer=kernel_initializer,
+                                    bn_epsilon=bn_epsilon, bn_momentum=bn_
