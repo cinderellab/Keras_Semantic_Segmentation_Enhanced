fdf864199fd1f365a94867fba6c52c30a874e94d
cinderellab <cinderela80@proton.me>
2021-01-28 22:29:29 +0000

    Pontoon: Update Slovenian (sl) localization of Thimble

--- core/nets/unets.py
+++ core/nets/unets.py
@@ -337,4 +337,6 @@ def MobileUNet(input_shape,
     x = DepthwiseSeparableConvBlock(x, 512, weight_decay=weight_decay, kernel_initializer=kernel_initializer,
                                     bn_epsilon=bn_epsilon, bn_momentum=bn_momentum)
     x = DepthwiseSeparableConvBlock(x, 512, weight_decay=weight_decay, kernel_initializer=kernel_initializer,
-                                    bn_epsilon=bn_epsilon, bn_momentum=bn_
+                                    bn_epsilon=bn_epsilon, bn_momentum=bn_momentum)
+    x = DepthwiseSeparableConvBlock(x, 512, weight_decay=weight_decay, kernel_initializer=kernel_initializer,
+                            
