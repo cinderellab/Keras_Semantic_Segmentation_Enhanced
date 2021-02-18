a3717c15490c0c688431af3a25bb79d7fba605db
cinderellab <cinderela80@proton.me>
2021-02-18 21:07:03 +0000

    Spelling correction

--- core/nets/unets.py
+++ core/nets/unets.py
@@ -349,4 +349,7 @@ def MobileUNet(input_shape,
                                     bn_epsilon=bn_epsilon, bn_momentum=bn_momentum)
     x = DepthwiseSeparableConvBlock(x, 512, weight_decay=weight_decay, kernel_initializer=kernel_initializer,
                                     bn_epsilon=bn_epsilon, bn_momentum=bn_momentum)
-    x = DepthwiseSeparableConvBlock(x, 256, weight_decay=weight_decay, kernel_initializer
+    x = DepthwiseSeparableConvBlock(x, 256, weight_decay=weight_decay, kernel_initializer=kernel_initializer,
+                                    bn_epsilon=bn_epsilon, bn_momentum=bn_momentum)
+    if has_skip:
+        x = Add()([x, skip_3
