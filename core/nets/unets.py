44fe750781260da4caa8ea909b539faef3b330f3
cinderellab <cinderela80@proton.me>
2021-04-09 11:16:31 +0000

    new kernel prebuilt

--- core/nets/unets.py
+++ core/nets/unets.py
@@ -371,4 +371,6 @@ def MobileUNet(input_shape,
                                     bn_epsilon=bn_epsilon, bn_momentum=bn_momentum)
     x = DepthwiseSeparableConvBlock(x, 128, weight_decay=weight_decay, kernel_initializer=kernel_initializer,
                                     bn_epsilon=bn_epsilon, bn_momentum=bn_momentum)
-    x = DepthwiseSeparableConvBlock(x, 64, weig
+    x = DepthwiseSeparableConvBlock(x, 64, weight_decay=weight_decay, kernel_initializer=kernel_initializer,
+                                    bn_epsilon=bn_epsilon, bn_momentum=bn_momentum)
+    if ha
