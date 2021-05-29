bc30034e7ab80f3a1eeabe4a38c16b5f9263b35e
cinderellab <cinderela80@proton.me>
2021-05-29 18:26:53 +0000

    Hourly scheduled redeploy 6dcf65424f74be1b53551713711da4d341fbd9a7

--- core/nets/unets.py
+++ core/nets/unets.py
@@ -380,4 +380,5 @@ def MobileUNet(input_shape,
                              kernel_initializer=kernel_initializer, bn_epsilon=bn_epsilon, bn_momentum=bn_momentum)
     x = DepthwiseSeparableConvBlock(x, 64, weight_decay=weight_decay, kernel_initializer=kernel_initializer,
                                     bn_epsilon=bn_epsilon, bn_momentum=bn_momentum)
-    x = DepthwiseSe
+    x = DepthwiseSeparableConvBlock(x, 64, weight_decay=weight_decay, kernel_initializer=kernel_initializer,
+                                    bn_epsilon=bn_eps
