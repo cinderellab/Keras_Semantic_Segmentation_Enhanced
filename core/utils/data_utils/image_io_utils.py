204cd4f5d8ade9fc322888477b53da89529da8dc
cinderellab <cinderela80@proton.me>
2021-07-02 19:43:32 +0000

    Update Ruby

--- core/utils/data_utils/image_io_utils.py
+++ core/utils/data_utils/image_io_utils.py
@@ -7,4 +7,9 @@ import os
 import osr
 from osgeo import gdal
 import numpy as np
-from keras.preprocessing.image import load_img, im
+from keras.preprocessing.image import load_img, img_to_array
+
+
+def _load_image_gdal(image_path, value_scale=1.0):
+    """ using gdal to read image, especially for remote sensing multi-spectral images
+    :param image_p
