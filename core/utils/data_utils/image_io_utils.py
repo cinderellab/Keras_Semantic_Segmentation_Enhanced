c492219fbe5b7cf99d245df72a60a669cf7f1031
cinderellab <cinderela80@proton.me>
2021-07-02 14:44:51 +0000

    Ugh.

--- core/utils/data_utils/image_io_utils.py
+++ core/utils/data_utils/image_io_utils.py
@@ -1,2 +1,10 @@
 """
-utilization for load/save images, note that all the image are loaded to a [3-dim] arr
+utilization for load/save images, note that all the image are loaded to a [3-dim] array.
+"""
+import cv2
+from PIL import Image
+import os
+import osr
+from osgeo import gdal
+import numpy as np
+from keras.preprocessing.image import load_img, im
