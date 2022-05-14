dd6bf0ef521beae350f8e69148b78d6395ace4f8
cinderellab <cinderela80@proton.me>
2022-05-14 18:33:55 +0000

    Yup.

--- core/utils/data_utils/label_transform_utils.py
+++ core/utils/data_utils/label_transform_utils.py
@@ -1,3 +1,5 @@
 import numpy as np
 
-def color_to_index(color_array, color_mappin
+def color_to_index(color_array, color_mapping, to_sparse=True):
+    """ convert colorful label array to label index
+        :param color_array: array of shape (height, width) or (height, width, 
