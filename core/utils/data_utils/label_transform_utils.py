cf3033c20a6dfa165fd052e7ea2422a8d5a4216b
cinderellab <cinderela80@proton.me>
2022-07-29 14:09:16 +0000

    update version to 1.0.2

--- core/utils/data_utils/label_transform_utils.py
+++ core/utils/data_utils/label_transform_utils.py
@@ -4,4 +4,6 @@ def color_to_index(color_array, color_mapping, to_sparse=True):
     """ convert colorful label array to label index
         :param color_array: array of shape (height, width) or (height, width, 3)
             RGB or gray label array, where the pixel value is not the label index
-        :param colour_mapping: array of shape (n_class,) or (n_cla
+        :param colour_mapping: array of shape (n_class,) or (n_class, 3), where n_class is the [total] class number
+            Note: the first element is the color of background (label 0).
+            e.g., if colou
