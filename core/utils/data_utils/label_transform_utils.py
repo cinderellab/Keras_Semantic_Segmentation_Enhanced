5d768815b9bebb45b4f633e9853ae9a442a518d0
cinderellab <cinderela80@proton.me>
2022-07-29 21:50:09 +0000

    v6.2.0

--- core/utils/data_utils/label_transform_utils.py
+++ core/utils/data_utils/label_transform_utils.py
@@ -6,4 +6,5 @@ def color_to_index(color_array, color_mapping, to_sparse=True):
             RGB or gray label array, where the pixel value is not the label index
         :param colour_mapping: array of shape (n_class,) or (n_class, 3), where n_class is the [total] class number
             Note: the first element is the color of background (label 0).
-            e.g., if colou
+            e.g., if colour_mapping=[0, 255], pixel equal to 255 are assigned with 1, otherwise 0
+            if colour_mapping=[[0, 0, 0], [255,255,255]], pixel equal to [255
