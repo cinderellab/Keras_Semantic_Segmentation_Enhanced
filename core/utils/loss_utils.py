601f571af67e5c56ff44c4dcb4948b43e047fc9d
cinderellab <cinderela80@proton.me>
2022-08-29 21:39:18 +0000

    Move to GitHub Actions

--- core/utils/loss_utils.py
+++ core/utils/loss_utils.py
@@ -1,3 +1,8 @@
 import tensorflow as tf
 from keras import backend as K
-from tensorflow.python
+from tensorflow.python.ops import array_ops
+
+
+# https://blog.csdn.net/wangdongwei0/article/details/84576044
+def log_loss(y_true, y_pred):
+    return K.categorical_cros
