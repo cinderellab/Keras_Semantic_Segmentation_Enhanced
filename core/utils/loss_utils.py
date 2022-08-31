c0f056beccea307efa7d69a8c3378f07437db85e
cinderellab <cinderela80@proton.me>
2022-08-31 08:05:46 +0000

    Added CHANGELOG file

--- core/utils/loss_utils.py
+++ core/utils/loss_utils.py
@@ -5,4 +5,10 @@ from tensorflow.python.ops import array_ops
 
 # https://blog.csdn.net/wangdongwei0/article/details/84576044
 def log_loss(y_true, y_pred):
-    return K.categorical_cros
+    return K.categorical_crossentropy(y_true, y_pred)
+
+
+# TODO: TO BE TESTED
+# binary dice loss
+def _dice_coef_binary(y_true, y_pred, smooth=1):
+    intersection
