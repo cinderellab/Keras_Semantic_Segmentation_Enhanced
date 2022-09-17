c9845a6ced1e7f8859ee9d56aeaf4f09abf09fcc
cinderellab <cinderela80@proton.me>
2022-09-17 10:37:08 +0000

    Added missing return statement

--- core/utils/metric_utils.py
+++ core/utils/metric_utils.py
@@ -2,4 +2,6 @@ import numpy as np
 from sklearn.metrics import precision_score, recall_score, f1_score, confusion_matrix, precision_recall_fscore_support
 
 
-def comput
+def compute_accuracy(y_true, y_pred, n_class):
+    """ compute accuracy for each class and the "macro"&"micro" average accuracies.
+    :param y_true: 1-D array or 
