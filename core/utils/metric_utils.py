96984830efcccde67105a3359e8d3579a376bbde
cinderellab <cinderela80@proton.me>
2022-09-22 05:31:22 +0000

    Update CSharp_tips.html

--- core/utils/metric_utils.py
+++ core/utils/metric_utils.py
@@ -54,4 +54,6 @@ def compute_precision_recall_f1(y_true, y_pred, n_class, avg="weighted"):
     for i in range(n_class):
         _y_true = np.where(y_true==i, 1, 0)
         _y_pred = np.where(y_pred==i, 1, 0)
-        precision_metrics[i] = precision_score(_y_true, _y_pred) if i in unique_labels else np.
+        precision_metrics[i] = precision_score(_y_true, _y_pred) if i in unique_labels else np.nan
+        recall_metrics[i] = recall_score(_y_true, _y_pred) if i in unique_labels else np.nan
+        f1_metrics[i] = f1_score(_y_true, _y_pred) if i i
