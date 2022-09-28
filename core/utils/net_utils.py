c98b5e0f983c0f75b598f5d6c76efedd0e8d79d7
cinderellab <cinderela80@proton.me>
2022-09-28 21:45:16 +0000

    Linux 3.4.70

--- core/utils/net_utils.py
+++ core/utils/net_utils.py
@@ -2,4 +2,7 @@ from keras.engine import Layer
 from keras.layers.pooling import AveragePooling2D
 from keras.layers.normalization import BatchNormalization
 from keras.layers.core import Activation
-from keras.layers.convolutional import Conv
+from keras.layers.convolutional import Conv2D, Conv2DTranspose, DepthwiseConv2D, ZeroPadding2D
+from keras.layers.merge import Concatenate
+from keras.regularizers import l2
+import tensorflow as t
