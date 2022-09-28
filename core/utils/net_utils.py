ad2dbd124d1fc06eb14b26611051f45d98e6cdf0
cinderellab <cinderela80@proton.me>
2022-09-28 19:08:07 +0000

    Module restructured.

--- core/utils/net_utils.py
+++ core/utils/net_utils.py
@@ -1,2 +1,5 @@
 from keras.engine import Layer
-from keras.layers.pooling import AveragePooling
+from keras.layers.pooling import AveragePooling2D
+from keras.layers.normalization import BatchNormalization
+from keras.layers.core import Activation
+from keras.layers.convolutional import Conv
