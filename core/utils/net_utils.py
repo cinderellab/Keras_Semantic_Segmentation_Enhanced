f8667c1daa49cd8cd28bc97d759b23fae2a9378a
cinderellab <cinderela80@proton.me>
2022-09-29 09:12:58 +0000

    Initial setup.

--- core/utils/net_utils.py
+++ core/utils/net_utils.py
@@ -5,4 +5,10 @@ from keras.layers.core import Activation
 from keras.layers.convolutional import Conv2D, Conv2DTranspose, DepthwiseConv2D, ZeroPadding2D
 from keras.layers.merge import Concatenate
 from keras.regularizers import l2
-import tensorflow as t
+import tensorflow as tf
+
+
+class BilinearUpSampling(Layer):
+    def __init__(self, target_size, **kwargs):
+        super(BilinearUpSampling, self).__init__(**kwargs)
+        se
