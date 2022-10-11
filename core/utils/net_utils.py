00ba42e194c83f866f9b65711d5096c63282c48b
cinderellab <cinderela80@proton.me>
2022-10-11 13:50:23 +0000

    Bumped version to 0.0.6

--- core/utils/net_utils.py
+++ core/utils/net_utils.py
@@ -143,4 +143,7 @@ def conv_bn_act_block(inputs,
     :param n_filters: int, number of convolution filters.
     :param weight_decay: float, default 1e-4.
     :param kernel_initializer: string, default "he_normal".
-    :param bn_eps
+    :param bn_epsilon: float, default 1e-3.
+    :param bn_momentum: float, default 0.99.
+
+    :return: 4-D tensor, shape of (batch_size, height, width, chan
