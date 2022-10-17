6b99115563ce39017bf7ef337fa6fb3c4829ba2a
cinderellab <cinderela80@proton.me>
2022-10-17 18:12:06 +0000

    Alexa Cookbook updates

--- core/utils/net_utils.py
+++ core/utils/net_utils.py
@@ -200,4 +200,8 @@ def bn_act_convtranspose(inputs,
         :param weight_decay: float, default 1e-4.
         :param kernel_initializer: string, default "he_normal".
         :param bn_epsilon: float, default 1e-3.
-        :param bn_momentum: float, default 
+        :param bn_momentum: float, default 0.99.
+
+        :return: 4-D tensor, shape of (batch_size, height, width, channel).
+        """
+    x = BatchNormalization(epsilon=bn_epsilon, momentum=bn
