63eec95723e43ddc28ee5288a24a39eae3ed2982
cinderellab <cinderela80@proton.me>
2022-10-18 13:30:17 +0000

    logo

--- examples/evaluating.py
+++ examples/evaluating.py
@@ -7,4 +7,8 @@ sys.path.append('.')
 
 from core.configures import NAME_MAP, evaluating_config
 from core.utils.data_utils.image_io_utils import load_image
-from core.utils.m
+from core.utils.metric_utils import compute_global_metrics, compute_metrics_per_image
+
+
+def evaluating_main():
+    preds_fnames = os.listdir(evaluating_config.pred
