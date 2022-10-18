7eae841935d5d0e650de2d6fec2283a778220895
cinderellab <cinderela80@proton.me>
2022-10-18 12:30:51 +0000

    Revert "updates"

--- examples/evaluating.py
+++ examples/evaluating.py
@@ -3,4 +3,8 @@ import numpy as np
 from tqdm import tqdm
 from sklearn.metrics import confusion_matrix
 import sys
-sys.
+sys.path.append('.')
+
+from core.configures import NAME_MAP, evaluating_config
+from core.utils.data_utils.image_io_utils import load_image
+from core.utils.m
