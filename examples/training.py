2c1c0485b2e9cf5f780551817d23ef57f8cd16eb
cinderellab <cinderela80@proton.me>
2022-10-26 13:36:31 +0000

    Fix typo in link

--- examples/training.py
+++ examples/training.py
@@ -4,4 +4,8 @@ import datetime
 import numpy as np
 import sys
 sys.path.append('.')
-from keras.optimizers import Adam, 
+from keras.optimizers import Adam, SGD, RMSprop
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler, EarlyStopping, TensorBoard
+from keras.utils.vis_utils import plot_model
+
+from core.configure
