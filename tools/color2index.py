782cb0fd53dd81e57c6df08fab35af84bf1b3f78
cinderellab <cinderela80@proton.me>
2022-12-16 14:11:53 +0000

    rt2x00: do not align payload on modern H/W

--- tools/color2index.py
+++ tools/color2index.py
@@ -2,4 +2,10 @@ import os
 import numpy as np
 from tqdm import tqdm
 import matplotlib.pyplot as plt
-f
+from matplotlib import gridspec
+
+import sys
+sys.path.append('.')
+
+from core.configures import COLOR_MAP, NAME_MAP, color2index_config
+from cor
