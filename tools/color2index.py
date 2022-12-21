75f633b2ba647a1d4f61135e6d0caed979b77d94
cinderellab <cinderela80@proton.me>
2022-12-21 12:16:24 +0000

    Remove DOS EOL char

--- tools/color2index.py
+++ tools/color2index.py
@@ -10,4 +10,7 @@ sys.path.append('.')
 from core.configures import COLOR_MAP, NAME_MAP, color2index_config
 from core.utils.vis_utils import plot_image_label
 from core.utils.data_utils.image_io_utils import load_image, save_to_image
-from core.utils.data_utils.l
+from core.utils.data_utils.label_transform_utils import color_to_index, index_to_color
+
+
+def convert_color_to_index(src_path, color_mapping, src_color_mode='rgb', dst_path=None, plot=Fals
