d5526aa0fa9384c8f24460c6f215e3c7c12db45d
cinderellab <cinderela80@proton.me>
2023-02-26 08:07:30 +0000

    0.0.69

--- tools/generate_dataset.py
+++ tools/generate_dataset.py
@@ -30,4 +30,5 @@ def generate_dataset_random(image_paths,
         label = load_image(label_path, is_gray=label_is_gray, use_gdal=use_gdal)
         image_height, image_width, _ = image.shape
         image_tag = os.path.basename(image_path).split('.')[0]
-  
+        print('%s: sampling from [%s]...' % (datetime.datetime.now().strftime('%y-%m-%d %H:%M:%S'), image_path))
+        # if the source image/label is too small, pad it with z
