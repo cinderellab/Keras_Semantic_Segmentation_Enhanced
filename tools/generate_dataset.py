075dfe22023b51174f5bde4d826c3cca4fa47a72
cinderellab <cinderela80@proton.me>
2023-03-18 07:22:45 +0000

    Edited comments

--- tools/generate_dataset.py
+++ tools/generate_dataset.py
@@ -50,4 +50,7 @@ def generate_dataset_random(image_paths,
             if src_roi.shape[0]!=img_h or src_roi.shape[1]!=img_w or label_roi.shape[0]!=img_h or label_roi.shape[1]!=img_w:
                 continue
             if not use_gdal:
-                save_to_image(src_roi.astype(np.uint8), '{}/image/{}_{}.png'.format(dst_dir, image_tag, 
+                save_to_image(src_roi.astype(np.uint8), '{}/image/{}_{}.png'.format(dst_dir, image_tag, l_count))
+                save_to_image(label_roi.astype(np.uint8), '{}/label/{}_{}.png'.format(dst_dir, image_tag, l_count))
+            else:
+                save_to
