1b5e519739930ca08e7fc8cb1e1eb02c308a1eae
cinderellab <cinderela80@proton.me>
2023-05-04 10:49:26 +0000

    biuld angular-seed

--- tools/generate_dataset.py
+++ tools/generate_dataset.py
@@ -95,4 +95,7 @@ def generate_dataset_scan(image_paths,
                 if src_roi.shape[0]!=img_h or src_roi.shape[1]!=img_w:
                     continue
 
-                if src_roi.shape[0]!=img_h or src_roi.shape[1]!
+                if src_roi.shape[0]!=img_h or src_roi.shape[1]!=img_w or label_roi.shape[0]!=img_h or label_roi.shape[1]!=img_w:
+                    continue
+                # save sample images
+    
